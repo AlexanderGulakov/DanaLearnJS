6ed223f2198f0598dc83b695ef3eacaf4fd36d4a
Alexander Gulakov <alexander.gulakov@pettersonapps.com>
2022-06-06 19:20:30 +0300

    fix liniyni1

---
+++ liniyni1.js
@@ -0,0 +1,4 @@
+let widthOfScreen = parseFloat(prompt("Write the width of the screen", 0));
+let quantity = parseInt(prompt("Enter the quantity elements", 0));
+let widthOfElements = widthOfScreen / quantity;
+alert("width Of Elements =" + widthOfElements.toFixed (5));
