1b543470e4fbf903440f460b3e37e5a9313834b5
Alexander Gulakov <alexander.gulakov@pettersonapps.com>
2022-06-06 19:28:24 +0300

    liniyni1 fix2

--- hello.js
+++
@@ -1,7 +0,0 @@
-
-
-
-let widthOfScreen = parseInt(prompt("Write the width of the screen", 0));
-let quantity = parseInt(prompt("Enter the quantity elements", 0));
-let widthOfElements = widthOfScreen / quantity;
-alert("width Of Elements =" + widthOfElements);
