fc18486d8ce5466f7ad722582721fd3ecdb5e422
Alex Gul <alexgul30@gmail.com>
2022-06-06 18:37:49 +0300

    first task width of elements

---
+++ hello.js
@@ -0,0 +1,7 @@
+
+
+
+let widthOfScreen = parseInt(prompt("Write the width of the screen", 0));
+let quantity = parseInt(prompt("Enter the quantity elements", 0));
+let widthOfElements = widthOfScreen / quantity;
+alert("width Of Elements =" + widthOfElements);
