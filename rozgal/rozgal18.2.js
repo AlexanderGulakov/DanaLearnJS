e684b6fbafcbda907752b7148971d11e3ecf3602
Alex Gul <alexgul30@gmail.com>
2022-06-21 18:22:56 +0300

    rozgal18.1 i rozgal18.2

--- rozgal/rozgal18.2.js
+++ rozgal/rozgal18.2.js
@@ -9,55 +9,50 @@
 
 // Задача 18.1. Модифікувати попередню задачу з можливістю після пострілу переміщення корабля у випадковим чином вибрану сусідню клітинку 
 // (якщо звичайно корабель не було потоплено за першим разом).
+// Задача 18.2. Модифікувати попередню задачу таким чином, що кораблем керує користувач, а комп’ютер стріляє. 
 
-
-let shipsPosition = Math.ceil(Math.random() * 5);
-console.log('ships position', shipsPosition);
-const fieldLength = 5;
-let shotAtShip = parseInt(prompt(" Enter the square number"));
+let shipsPosition = parseInt(prompt(" Enter the position of the ship"))
+console.log('ships position', shipsPosition)
+// const fieldLength = 5
+let shotAtShip = Math.ceil(Math.random() * 5)
+console.log('shot at ship', shotAtShip)
+alert(`Random Number =  ${shotAtShip}`)
 
 if (shotAtShip === shipsPosition) {
-    alert(`Well done. You sank the ship. You win. Random Number =  ${shipsPosition}`)
+    alert(`Well done. You sank the ship. You win.`)
 }
 else {
     if (Math.abs(shotAtShip - shipsPosition) === 1) {
         alert("The ship is damaged.")
     }
-    shotAtShip = parseInt(prompt(`Try again. Enter the square number`))
-    let changeShipPosition = Math.random() 
-    if(changeShipPosition > 0 && changeShipPosition < 0.50) {
-        shipsPosition = shipsPosition + Math.ceil(changeShipPosition)
-    console.log('ships position+', shipsPosition);
+    changeShipPosition = parseInt(prompt(" Enter the action that will move the ship one cell forward or backward + or -"))
+    if (changeShipPosition = "+") {
+        shipsPosition = shipsPosition + 1
+        console.log('ships position + ', shipsPosition)
     }
-     else if (changeShipPosition > 0.50 && changeShipPosition<  1)
-    {
-      shipsPosition = shipsPosition - Math.ceil(changeShipPosition)
-      console.log('ships position-', shipsPosition);
+    else if (changeShipPosition = "-") {
+        shipsPosition = shipsPosition - 1
+        console.log('ships position - ', shipsPosition)
     }
-      if (shipsPosition > 5) {
-        shipsPosition =1
-        console.log('ships position3', shipsPosition);
+    if (shipsPosition > 5) {
+        shipsPosition = 1
+        console.log('ships position >5 ', shipsPosition)
     }
     else if (shipsPosition < 1) {
         shipsPosition = 5
-        console.log('ships position3', shipsPosition);
+        console.log('ships position <1 ', shipsPosition)
     }
-   
+    shotAtShip = Math.ceil(Math.random() * 5)
+    alert(`Random Number =  ${shotAtShip}`)
+    console.log('random number2 ', shotAtShip)
+
     if (shotAtShip === shipsPosition) {
-        alert(`Well done. You sank the ship. You win. Random Number =  ${shipsPosition}`)
+        alert(`Well done. You sank the ship. You win.`)
     }
     else if (Math.abs(shotAtShip - shipsPosition) === 1) {
-        alert(`The ship is damaged. You win. Random Number =  ${shipsPosition}`)
+        alert(`The ship is damaged. You win. `)
     }
     else {
-        alert(`You lost.Random Number = ${shipsPosition} `)
+        alert(`You lost. `)
     }
 }
-
-
-
-
-
-
-// Math.floor(Math.random() * (max - min)) + min;
-    // shipsPosition = (5 + shipsPosition ) % 5 + 1
