9d58bed3de641defaf4e29e4b67333e5942057b1
Alex Gul <alexgul30@gmail.com>
2022-07-10 18:22:52 +0300

    rashists warship gfy

--- maks.js
+++ maks.js
@@ -1,89 +1,31 @@
-/*    10.	Користувач вводить числа поки не натисне відміну. Знайти 
-1.	кількість парних; ОК
-2.	добуток непарних; ОК
-3.	суму двоцифрових; ОК
-4.	середнє арифметичне додатних; ОК
-5.	найбільше від’ємне;
-6.	кількість більших за перше число; ОК
-7.	середнє геометричне додатних; ОК
-8.	найменше з них;
-9.	кількість трицифрових; ОК
-10.	добуток кратних 5;     ОК */
+/*31. Морський бій. Комп’ютер випадковим чином розміщує одиночний корамель на полі 5*5. З клавіатури поступово вводяться координати пострілу поки корабель не буде потоплено. Спробуйте відоразити гру графічно.*/
 
-let usersNumber = 0;
-let evenCount = 0;
-let oddsProduct = 1;
-let twoDigitsSumm = 0;
-let arithmeticsPositivNumbers = 0;
-let positivNumbersCount = 0;
-let biggestNegativeNumber = null;
-let biggerThenFirstNumbersCount = 0;
-let firstNumber;
-let geometricsPositiv = 1;
-let smallestPositiveNumber = null;
-let amountThreeDigits = 0;
-let productFivesMultiple = 1;
+let fieldsRow = +prompt (`Enter the width of the field`);
+let fieldsColumn = +prompt (`Enter the height of the field`);
+document.write(`<h1 text-align="center">Black Sea</h1>`);
+document.write(`<table border=3 class="center">`);
+let positionX = 3
+let positionY = 4
+let isGoesToHell = false
+let shotX
+let shotY
+for (let i = 0; i < fieldsRow; i++) {
+  document.write(`<tr">`);
+  for (let j = 0; j < fieldsColumn; j++) {
 
-for (let defaultNumber = 0; usersNumber !== undefined; defaultNumber++) {
-  usersNumber = prompt("Type in the number") || undefined;
-  if (usersNumber === undefined) {
-    break;
+    document.write(`<td id=${i+1}${j+1} class="city"></td>`);
   }
-  if (usersNumber % 2 === 0) {
-    evenCount++; // 1. кількість парних
-  } else {
-    oddsProduct = oddsProduct * usersNumber; // 2. добуток непарних
-  }
-  if (
-    (usersNumber >= 10 && usersNumber <= 99) ||
-    (usersNumber <= -10 && usersNumber >= -99)
-  ) {
-    twoDigitsSumm = +twoDigitsSumm + +usersNumber; // 3. сума двоцифрових
-  }
-  if (usersNumber > 0) {
-    positivNumbersCount++;
-    arithmeticsPositivNumbers = +arithmeticsPositivNumbers + +usersNumber; // 4. середнє арифметичне
-    geometricsPositiv = geometricsPositiv * usersNumber; // 7. середнє геометричне
-    if (
-      smallestPositiveNumber === null ||
-      smallestPositiveNumber > usersNumber
-    ) {
-      smallestPositiveNumber = +usersNumber; // 8. найменше з додатних
-      console.log("smallestE", smallestPositiveNumber);
-    }
-  }
-  if (usersNumber < 0) {    //5. найбільше від'ємне
-    if (biggestNegativeNumber === null || biggestNegativeNumber < usersNumber)
-      biggestNegativeNumber = +usersNumber;
-      console.log("biggestE", biggestNegativeNumber);
-  }
-  if (defaultNumber === 0) {
-    firstNumber = +usersNumber;
-  }
-  if (firstNumber < usersNumber) {
-    biggerThenFirstNumbersCount++; // 6. кількість більших за перше число
-  }
-  if (
-    (usersNumber >= 100 && usersNumber <= 999) ||
-    (usersNumber <= -100 && usersNumber >= -999)
-  ) {
-    amountThreeDigits++; // 9. кількість трицифрових
-  }
-  if (usersNumber % 5 === 0) {
-    productFivesMultiple = productFivesMultiple * usersNumber; // 10. добуток кратних п'яти
-  }
+  document.write(`</tr>`);
 }
-arithmeticsPositivNumbers = arithmeticsPositivNumbers / positivNumbersCount; // 4. середнє арифметичне
-geometricsPositiv = Math.pow(geometricsPositiv, 1 / positivNumbersCount); // 7. середнє геометричне
-document.write(
-  `<b>Evens count</b> = <B><I>${evenCount}</I></B> <br><HR>
-  <b>Odds product</b> = <B><I>${oddsProduct}</I></B> <br><HR>
-  <b>Summ of two digit numbers</b> = <B><I>${twoDigitsSumm}</I></B> <br><HR>
-  <b>Arithmetic mean of positive numbers</b> = <B><I>${arithmeticsPositivNumbers}</I></B> <br><HR> 
-  <b>The count of bigger numbers then first user number</b> = <B><I>${biggerThenFirstNumbersCount}</I></B> <br><HR>
-  <b>The geometric mean of positive numbers</b> = <B><I>${geometricsPositiv}</I></B> <br><HR>
-  <b>The count of three digit numbers</b> = <B><I>${amountThreeDigits}</I></B> <br><HR>
-  <b>The bigest negative number</b> = <B><I>${biggestNegativeNumber}</I></B> <br><HR> 
-  <b>The smallest positive number</b> = <B><I>${smallestPositiveNumber}</I></B> <br><HR>
-  <b>The product of multiple of five numbers</b> = <B><I>${productFivesMultiple}</I></B><br><HR>`
-);
+document.write(`</table>`);
+alert(`Commander, we need to sunk this fucking russian warship! Neptune is charged! Provide the coordinates of this fish feed:`)
+do{
+  shotX = +prompt (`PositionX:`);
+  shotY = +prompt (`PositionY:`);
+  if(shotX === positionX && shotY === positionY){
+    isGoesToHell = true
+    document.getElementById(`${shotX}${shotY}`).style.backgroundImage = "url('1.jpg')";
+  }
+} while(!isGoesToHell)
+alert(`Well done, Commander, russian warship - go fuck yourself!`)
+// щось таке? якась фігня) як же ж тоді зафарбовувати влучання
--- test.js
+++ test.js
@@ -1,21 +1,26 @@
-if (usersNumber > 0) {
-    if (smallestPositiveNumber === null)
-        smallestPositiveNumber = +usersNumber
-    else if (usersNumber < smallestPositiveNumber)
-        smallestPositiveNumber = +usersNumber
-}
+/*28. З клавіатури вводяться поступово числа. Знайти два найбільші числа. Ввід здійснюється до тих пір, поки різниця між такими числами є більшою за 5.*/
 
-
-if (usersNumber > 0) {
-    if (smallestPositiveNumber === null) {
-        smallestPositiveNumber = +usersNumber; // 8. найменше з додатних
-        console.log("small", smallestPositiveNumber);
-    };
-    if (smallestPositiveNumber < usersNumber) {
-        // 8. найменше з додатних
-        console.log("smallest", smallestPositiveNumber);
-    } else {
-        smallestPositiveNumber = +usersNumber;
-        console.log("smallestE", smallestPositiveNumber);
-    };
-}
+let usersNumberSecond;
+let secondBiggestNumber = 0;
+let firstBiggestNumber = +prompt(`Enter number`) || 0;
+do {
+  usersNumberSecond = +prompt(`Enter number`) || 6;
+  if (!secondBiggestNumber) {
+      secondBiggestNumber = usersNumberSecond;
+  };
+  if (usersNumberSecond > firstBiggestNumber) {
+      if (usersNumberSecond > secondBiggestNumber) {
+          if (secondBiggestNumber > firstBiggestNumber) {
+              firstBiggestNumber = usersNumberSecond;
+              continue;
+          }
+          if (secondBiggestNumber < firstBiggestNumber) {
+              secondBiggestNumber = usersNumberSecond;
+          };
+      }
+      if (usersNumberSecond < secondBiggestNumber) {
+          firstBiggestNumber = usersNumberSecond;
+      };
+  };
+    console.log("first", firstBiggestNumber, "second", secondBiggestNumber);
+} while (Math.abs(secondBiggestNumber - firstBiggestNumber) > 5);
