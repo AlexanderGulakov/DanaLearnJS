008b8093183a8b36033854f74d1c55bf7ddb17d2
Alexander Gulakov <alexander.gulakov@pettersonapps.com>
2022-07-19 20:55:19 +0300

    cycles22

--- cycles/cycles22.js
+++ cycles/cycles22.js
@@ -3,55 +3,62 @@
 //  (застосувати бінарний пошук, на кожному кроці якого інтервал пошуку зменшується на 2 
 //     шляхом порівняння з елементом, який знаходиться у центрі поточного інтервалу).
 
+
+
+let startInterval = 1
 let endInterval = +prompt("1.Guess the number.\n 2.Interval from 1 to maximum number. Enter the maximum number. ")
+let midInterval = Math.floor((startInterval + endInterval) / 2)
+console.log('midInterval  ', midInterval)
 let guessedNumber;
 let result;
-let startInterval = 1
-let midInterval;
-midInterval = Math.floor((startInterval + endInterval) / 2)
-console.log('midInterval  ', midInterval)
 do {
-
     guessedNumber = +prompt(` Select from the list :  \n1. Your number > ${midInterval}\n2. Your number < ${midInterval}\n3. Your number = ${midInterval} `)
+    console.log('guessedNumber  ', guessedNumber)
     switch (guessedNumber) {
-        case 1: result = Math.floor((midInterval + endInterval) / 2)  //75
-            // if (guessedNumber > midInterval) 
+        case 1:
+            startInterval = midInterval
+            midInterval = Math.floor((midInterval + endInterval) / 2)
+            console.log('case1  ', midInterval)
             break;
-        case 2: result = Math.floor((midInterval + result)) / 2    //60<75
-            //  if (guessedNumber < midInterval) 
+        case 2:
+            endInterval = midInterval
+            midInterval = Math.floor((startInterval + midInterval) / 2)
+            console.log('case2  ', midInterval)
             break;
         case 3: midInterval = ` Perfectly guessed number = ${midInterval} `
+            console.log('case3  ', midInterval)
+            alert(midInterval)
             break;
+            default: amount = "The number is incorrect. Try again.";
     }
-    alert(midInterval)
-}
+    if (endInterval !== 3 && endInterval === (startInterval + 2) || midInterval === 1 ) {
+        console.log('if  ', midInterval)
+        alert(` Your guessed number${midInterval}`)
+        break;
+    }
+    // if (midInterval === endInterval){
+    //     alert(` Your guessed number${midInterval}`)
+    //     break;
+    // }
 
+}
 while (guessedNumber !== 3)
 
-
-
-
-
-
-// let endInterval = +prompt("1.Guess the number.\n 2.Interval from 1 to maximum number. Enter the maximum number. ")
-// let guessedNumber;
-// let result;
-// let startInterval = 1
-// let midInterval = Math.floor((startInterval + endInterval) / 2)
-// console.log('midInterval  ', midInterval)
-// do {
-//     guessedNumber = +prompt(` Select from the list :  \n1. Your number > ${midInterval}\n2. Your number < ${midInterval}\n3. Your number = ${midInterval} `)
-//     switch (guessedNumber) {
-//         case 1:  midInterval = Math.floor((midInterval + endInterval) / 2)
-//             // if (guessedNumber > midInterval)
-//             break;
-//         case 2:  midInterval = Math.floor((midInterval + startInterval)) / 2
-//             //  if (guessedNumber < midInterval)
-//             break;
-//         case 3: midInterval = ` Perfectly guessed number = ${midInterval} `
-//             break;
-//     }
-//     alert(midInterval)
+// if (guessedNumber === 1) {
+//     startInterval = midInterval
+// }
+// else {
+//     endInterval = midInterval
+// }
+// if (guessedNumber === 2) {
+//     endInterval = midInterval
+// }
+// else {
+//     startInterval = midInterval
 // }
 
-// while (guessedNumber !== 3)
+
+
+
+
+
