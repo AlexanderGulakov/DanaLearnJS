0e8681822c2a1a93eb9ee5a5a756f1c17792a183
Alexander Gulakov <alexander.gulakov@pettersonapps.com>
2022-07-27 22:05:34 +0300

    cycles31.1

--- cycles/cycles31.2.js
+++ cycles/cycles31.2.js
@@ -3,10 +3,19 @@
 //  Спробуйте відоразити гру графічно.
 
 
- 
-let ShipPositionRow = Math.ceil(Math.random() * 5)
-let ShipPositionColumn = Math.ceil(Math.random() * 5)
 
-
-let shotAtShipRow = +prompt("A shot at the ship. Row =")
-let shotAtShipColumn = +prompt(" Column = ")
+let shipPositionRow = Math.ceil(Math.random() * 5)
+console.log('ships position row', shipPositionRow)
+let shipPositionColumn = Math.ceil(Math.random() * 5)
+console.log('ships position column', shipPositionColumn)
+let shotAtShipRow;
+let shotAtShipColumn;
+do {
+    shotAtShipRow = +prompt("A shot at the ship. From 1 to 2. Row =")
+    shotAtShipColumn = +prompt(" A shot at the ship. From 1 to 2. Column = ")
+    if ((shotAtShipRow === shipPositionRow) && (shotAtShipColumn === shipPositionColumn)) {
+        alert(' You won.The ship sank.')
+        break;
+    }
+}
+while (true)
