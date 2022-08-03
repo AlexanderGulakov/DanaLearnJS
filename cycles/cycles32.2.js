c97fac010fc30d1823e0968024e35683ed0e54ce
Alexander Gulakov <alexander.gulakov@pettersonapps.com>
2022-08-03 21:10:32 +0300

    cycles32

--- cycles/cycles32.2.js
+++ cycles/cycles32.2.js
@@ -7,17 +7,17 @@
 
 
 
-let rabbitPosition = Math.ceil(Math.random() * 10)                 //розміщення зайця               
+let rabbitPosition = Math.floor(Math.random() * (10 - 1)) + 1;     //розміщення зайця     5     
+// return Math.floor(Math.random() * (max - min)) + min;
 console.log('rabbit position', rabbitPosition)
 let shotPosition;
 const fieldLength = 10
 let shiftRabbitPosition;
-let quantityPatron = +prompt(" Write the quantity of patron.")  // кількість патронів              
+let quantityPatron = +prompt(" Write the quantity of patron.")  // кількість патронів                    
 console.log('quantity patron prompt ', quantityPatron)
 
-
 do {
-    shotPosition = +prompt(" Write the position of the shot.From 1 to 10")    // позиція пострілу    
+    shotPosition = +prompt(" Write the position of the shot.From 1 to 10")    // позиція пострілу      
     console.log('shot', shotPosition)
     quantityPatron--                                                        // кількість патронів     
     console.log('quantityPatron', quantityPatron)
@@ -29,46 +29,26 @@ do {
         alert(`You won`)
         break;
     }
-    shiftRabbitPosition = Math.floor(Math.random() * (3 - (-3))) + (-3);    //стрибок зайця                
+    shiftRabbitPosition = Math.floor(Math.random() * (3 - (-3))) + (-3);    //стрибок зайця  на   +2    +3     +2    -3
     // return Math.floor(Math.random() * (max - min)) + min;
     console.log('rabbit position shift', shiftRabbitPosition)
-    rabbitPosition = rabbitPosition % fieldLength + shiftRabbitPosition                                  //                 
-    console.log('rabbit position %', rabbitPosition)
- 
-    
+
+    if ((rabbitPosition + shiftRabbitPosition) < 1) {                                 // 2 - (-3)= -1<1           
+        rabbitPosition = (rabbitPosition + fieldLength) + shiftRabbitPosition        //2 + 10= 12 + (- 3) = 9
+        console.log('rabbit position <1', rabbitPosition)
+    }
+    else if ((rabbitPosition + shiftRabbitPosition) > 10) {                          // 10+2=12     
+        rabbitPosition = (rabbitPosition + shiftRabbitPosition) % fieldLength       // 10+2= 12% 10 = 2
+        console.log('rabbit position >10', rabbitPosition)
+    }
+    else {
+        rabbitPosition = rabbitPosition + shiftRabbitPosition                      // 5 +2 = 7   7+3 = 10
+        console.log('rabbit position ', rabbitPosition)
+    }
+
 }
 while (true)
 // (shotPosition !== rabbitPosition)
 // alert(`You won`)
 
 
-
-
-
-
-
-// let rabbitPosition = Math.ceil(Math.random() * 10)                 //розміщення зайця
-// console.log('rabbit position', rabbitPosition)
-// let shotPosition;
-// let quantityPatron = +prompt(" Write the quantity of patron.")  // кількість патронів
-// console.log('quantity patron', quantityPatron)
-
-
-// do {
-//     shotPosition = +prompt(" Write the position of the shot.From 1 to 10")    // позиція пострілу
-//     quantityPatron --                                                        // кількість патронів
-//     console.log('quantityPatron', quantityPatron)
-//     if (quantityPatron === 0) {
-//         alert(`You lost. quantity of patron = 0`)
-//         break;
-//     }
-
-//     rabbitPosition = Math.floor(Math.random() * ((rabbitPosition + 3) - (rabbitPosition - 3))) + (rabbitPosition - 3);    //стрибок зайця
-//     // return Math.floor(Math.random() * (max - min)) + min;
-//     console.log('rabbit position', rabbitPosition)
-//     rabbitPosition = rabbitPosition % 10 +
-//     console.log('rabbit position %', rabbitPosition)
-
-// }
-// while (shotPosition !== rabbitPosition)
-// alert(`You won`)
