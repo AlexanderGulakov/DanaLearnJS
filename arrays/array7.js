eba0ff8a207af15a056f92e767fd5d29036b697f
Alex Gul <alexgul30@gmail.com>
2022-09-22 13:40:55 +0300

    array7

--- arrays/array7.js
+++ arrays/array7.js
@@ -4,31 +4,65 @@
 
 
 
-let array = [5, 7, 4, 2, 1, 9, 3, 4]  //length = 8
+let array = [1, 2, 3, 4, 5, 6, 7, 8]  //length = 8
 const newArray = []
+// newArray = [ 3, 4, 5, 6, 7, 8, 1, 2]
 console.log(`array ${array}`)
-const k = parseInt(prompt(" Shift the elements of the array to the right by k positions.K <" + (array.length), ""))   //1
+const k = parseInt(prompt(" Shift the elements of the array to the right by k positions.K <" + (array.length), ""))   //7
 
 
-for (let i = 0, j = array.length - k ; i < array.length; i++, j++) {  // i<8   i=0  j = 2
-    // j =  //8-2=6
-    newArray[j] = array[i]   //  newArray[6] = array[0]    [7]=[1]   [1]=[3]  [2]=[4]  [3]=[5] [4]=[6] [5]=[7]
+for (let i = 0, j = array.length - k; i < array.length; i++, j++) {  //  i=0  j =8-7 = 1   i<8 
+    //  j = 1 i = 0  | j = 2 i = 1 | j = 3 i = 2 |  j = 4 i = 3 |  j = 5 i = 4 |  j = 6 i = 5|  j = 7 i = 6 |  j = 8  i = 7
 
-    if (j=== array.length  ) { // j===8  
+    if (j === array.length) { // j===8  
         j = 0                  // j= 0           
-        newArray[j] = array[i] //  newArray[0] = array[2]
+        newArray[j] = array[i] //  newArray[0] = array[7]
         console.log(`j if ${j}`)
-        // if (k === 1) {
-        //     break;                // break
-        // }
-        // j++                    // j= 
-        // i++                    // i= 
+        console.log(`i if ${i}`)
+        if ((array.length - k) === 1) {   // 8-7 =1
+            break;                // break
+        }
+        j++                    // j= 
+        i++                    // i= 
 
     }
 
+    newArray[j] = array[i]   //  newArray[1] = array[0] | [2] =[1]   [3] =[2]  [4] =[3]   [5] =[4]   [6] =[5]   [7] =[6]
     console.log(`i ${i}`)
     console.log(`j ${j}`)
     console.log(`newArray[j] ${newArray[j]}`)
 }
 
 console.log(`newArray[j] ${newArray}`)
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0, j = array.length - k; i < array.length; i++, j++) {  //  i=0  j =8-7 = 1   i<8
+//     // j = 1  j = 2 i = 1  j = 3 i = 2   j = 4 i = 3    j = 5 i = 4    j = 6 i = 5    j = 7 i = 6   j = 8  i = 7
+
+//     if (j === array.length) { // j===8
+//         j = 0                  // j= 0
+//         newArray[j] = array[i] //  newArray[0] = array[7]
+//         console.log(`j if ${j}`)
+//         console.log(`i if ${i}`)
+//         if ((array.length - k) > 1) {
+//             j++                    // j=
+//             i++                    // i=
+
+//         }
+
+//     }
+//     newArray[j] = array[i]   //  newArray[1] = array[0]  [2] =[1]   [3] =[2]  [4] =[3]   [5] =[4]   [6] =[5]   [7] =[6]
+//     console.log(`i ${i}`)
+//     console.log(`j ${j}`)
+//     console.log(`newArray[j] ${newArray[j]}`)
+// }
+
+// console.log(`newArray[j] ${newArray}`)
