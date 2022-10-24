c1310aab47d2ae11df784814f39b1169c3f8d194
Alexander Gulakov <alexander.gulakov@pettersonapps.com>
2022-10-24 09:44:56 +0300

    methodsArr

--- methodsArr/arr21.js
+++ methodsArr/arr21.js
@@ -1,8 +1,26 @@
 
 // //21.	Задача. Дано масив цін товарів. Знайти сумарну вартість тих, які знаходяться в межах від 1000 до 2000.
 
-// const arrayPrices = [1500, 220, 1028, 4490, 1000, 2400, 150]
-// const priceMore = 1000
-// const priceLess = 2000
-// const amountOfNumbers = arrayPrices.reduce((amount, currentValue) => currentValue > priceMore && currentValue < priceLess ? amount+= currentValue : currentValue)
-// console.log(`Amount of numbers = ${amountOfNumbers}`)
+const arrayPrices = [1500, 220, 1028, 4490, 1000, 2400, 150]
+const priceMore = 1000
+const priceLess = 2000
+
+const amountOfNumbers = arrayPrices.reduce((amount, currentValue) => {
+    
+ if (currentValue > priceMore && currentValue < priceLess) {
+      amount += currentValue
+       }
+    return amount
+}
+)
+console.log(`Array of Prices = ${arrayPrices}`)
+console.log(`Amount of numbers = ${amountOfNumbers}`)
+
+
+
+
+
+
+
+
+ 
