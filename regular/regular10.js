574f5697d0491f713fe739da08e3331ff7f3baa7
Alex Gul <alexgul30@gmail.com>
2022-11-18 21:02:57 +0200

    regular10

--- regular/regular10.js
+++ regular/regular10.js
@@ -1,17 +1,44 @@
 //Задача 10. Дано рядок тексту. Підрахувати кількість двоцифрових чисел у рядку.
+ 
+const stringOfText = "23 29 Oleksandr 33 111 11 Daniella "
+const regular = /(^|[^0-9])[1-9][0-9]($|[^0-9])/g
 
-const arrOfString = ["123", "29", "Oleksandr", "33", "111", "11", "Daniella"]
-const regular = /\d{2}/g
- const getQuantityTwoDigitNumbers = (regular, array) => array.reduce((quantity, currentValue) =>{
-    if(regular.test(currentValue)){
-        quantity++
-    }
-    return quantity
- } , 0
+const newString = stringOfText.match(regular)
+console.log(`newString  = ${newString}`)
 
- )
- const newString = getQuantityTwoDigitNumbers(regular,arrOfString)
- console.log(`newString = ${newString}`)
+
+
+// const getQuantityTwoDigitNumbers = (string, regular) => {
+//     if (string.match(regular)) {
+//         quantity++
+//     }
+//     return quantity
+// }
+
+// const newString = getQuantityTwoDigitNumbers(stringOfText, regular)
+//     console.log(`quantity  = ${quantity}`)
+
+
+
+
+
+
+
+
+//const arrOfString = ["123", "29", "22Oleksandr", "33", "111", "11", "Daniella"]
+// const regular = /\d{1,2}/g
+// const regular = /\[0-9]+/g
+ 
+//  const getQuantityTwoDigitNumbers = (regular, array) => array.reduce((quantity, currentValue) =>{
+//     if(regular.test(currentValue)){
+//         quantity++
+//     }
+//     return quantity
+//  } , 0
+
+//  )
+//  const newString = getQuantityTwoDigitNumbers(regular,arrOfString)
+//  console.log(`newString = ${newString}`)
 
 
 
